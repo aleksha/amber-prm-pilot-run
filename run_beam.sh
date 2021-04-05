bc447e689e4ce82d0e8df7875f872dc94a50e22b
Alexey Dzyuba <a.dzyuba@gmail.com>
2021-04-05 13:22:07 +0300

    mac files managment

--- run_beam.sh
+++ run_beam.sh
@@ -1,6 +1,9 @@
 mkdir run_beam
-cp beamfile/rand_input.txt run_beam/input_g4.txt
-cp build_g4/exec_MUP build_g4/run.mac run_beam/
-cp configs/seeds/seed_beam run_beam/seed
+cp -v beamfile/rand_input.txt run_beam/input_g4.txt
+cp -v build_g4/exec_MUP run_beam/
+cp -v configs/mac/run_beam.mac run_beam/run.mac
+cp -v configs/seeds/seed_beam run_beam/seed
 cd run_beam
+echo "Starting beam app"
 ./exec_MUP run.mac >> inLOG
+echo "Done! (see inLOG for details)"
--- source/Run_Geant.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm *LOG;
-source /home/user/Sim/geant4.10.04.p02-install/bin/geant4.sh;
-make;
-./exec_MUP run.mac >> inLOG;
