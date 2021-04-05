b62eac8ab368a07abbb80c63bb5108d74d9b8798
Alexey Dzyuba <a.dzyuba@gmail.com>
2021-04-05 13:07:19 +0300

    seeds managemenet

--- run_beam.sh
+++ run_beam.sh
@@ -1,5 +1,6 @@
 mkdir run_beam
 cp beamfile/rand_input.txt run_beam/input_g4.txt
-cp build_g4/exec_MUP build_g4/run.mac build_g4/seed run_beam/
+cp build_g4/exec_MUP build_g4/run.mac run_beam/
+cp configs/seeds/seed_beam run_beam/seed
 cd run_beam
 ./exec_MUP run.mac >> inLOG
