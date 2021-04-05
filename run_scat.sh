677f4adfb4ee24de9d025d7837186c7fefa4d795
Alexey Dzyuba <a.dzyuba@gmail.com>
2021-04-05 18:23:07 +0300

    run scripts

---
+++ run_scat.sh
@@ -0,0 +1,9 @@
+mkdir run_beam
+cp -v beamfile/rand_input.txt run_beam/input_g4.txt
+cp -v build_g4/exec_MUP run_beam/
+cp -v configs/mac/run_beam.mac run_beam/run.mac
+cp -v configs/seeds/seed_beam run_beam/seed
+cd run_beam
+echo "Starting beam app"
+./exec_MUP run.mac >> inLOG
+echo "Done! (see inLOG for details)"
