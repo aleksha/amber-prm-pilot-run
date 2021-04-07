8f65d173962d3912be6635905823f655178ee383
Alexey Dzyuba <a.dzyuba@gmail.com>
2021-04-07 08:26:51 +0200

    remove conda activation frpm script

--- scripts/build_g4.sh
+++ scripts/build_g4.sh
@@ -1,7 +1,5 @@
 echo "\nprm: build Geant4 app \n"
 rm -rf build_g4
-conda deactivate
-conda activate g4-mc
 mkdir build_g4
 cd build_g4
 cmake -DGeant4_DIR=/home/adzyuba/miniconda3/envs/g4-mc/lib/Geant4-10.7.1/ ../source
